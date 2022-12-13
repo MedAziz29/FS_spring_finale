fe8d5832f989038e1b78cdc047e0e3f2c5ec3279
MedAziz29 <103658249+MedAziz29@users.noreply.github.com>
2022-12-13 23:02:41 +0100

    Add files via upload

---
+++ animateur/src/main/java/com/aziz/animateurs/security/SecParams.java
@@ -0,0 +1,7 @@
+package com.aziz.animateurs.security;
+
+public interface SecParams {
+	public static final long  EXP_TIME = 10*24*60*60*1000;
+	public static final String SECRET = "azizbh1";
+	public static final String PREFIX = "Bearer ";
+}
